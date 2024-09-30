44753456e3544235577785a798842fcde7dc3ad7
kentonla <116829705+kentonla@users.noreply.github.com>
2024-09-29 22:47:01 -0700

    Storing cards in deck using a vector instead of array

--- war.cpp
+++ war.cpp
@@ -10,118 +10,9 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <stdexcept>
 #include <ctime> 
 using namespace std;
-
-class Deck{
-    private:
-        // use vector to store cards in deck
-        vector<Card> pile;
-        int topCard;
-    // Deck constructor. Creates a deck of 52 cards
-    public:
-        Deck::Deck(){
-            int index = -1;
-            char rank;
-            char suit;
-            topCard = 52;
-        // use vector to store cards in deck
-            for(int s = 0; s < 4; s++){
-                switch (s) {
-                    case 0:
-                        suit = 'H';
-                        break;
-                    case 1:
-                        suit = 'D';
-                        break;
-                    case 2:
-                        suit = 'C';
-                        break;
-                    case 3:
-                        suit = 'S';
-                        break;
-                }
-                for(int r = 0; r < 13; r++){
-                    switch (r) {
-                        case 0:
-                            rank = 'A';
-                            break;
-                        case 1:
-                            rank = '2';
-                            break;
-                        case 2:
-                            rank = '3';
-                            break;
-                        case 3:
-                            rank = '4';
-                            break;
-                        case 4:
-                            rank = '5';
-                            break;
-                        case 5:
-                            rank = '6';
-                            break;
-                        case 6:
-                            rank = '7';
-                            break;
-                        case 7:
-                            rank = '8';
-                            break;
-                        case 8:
-                            rank = '9';
-                            break;
-                        case 9:
-                            rank = 'T';
-                            break;
-                        case 10:
-                            rank = 'J';
-                            break;
-                        case 11:
-                            rank = 'Q';
-                            break;
-                        case 12:
-                            rank = 'K';
-                            break;
-                    }
-                    // Card currentCard(rank, suit);
-                    pile.push_back(Card(rank, suit));
-                }
-            }
-        }
-
-        // Deals a card by taking a card from the deck
-        // Deal a card if possible, otherwise Raise exception if # of games to play is > cards in deck
-        Card Deck::deal(){
-            return pile[--topCard];
-        }
-
-        //Shows all the cards in the deck
-        void Deck::print(){
-            for (int i = 0; i < topCard; i++){
-                if (i%13 == 0 && i != 0){
-                    cout << "\n";
-                }
-                pile[i].print();
-                cout << " ";
-            }
-            cout << endl << endl;
-        }
-
-        //Shuffles the cards in the deck
-        void Deck::shuffle(){
-            for (int i = 0; i < topCard; i++){
-                int random = rand() % topCard;
-                Card temp = pile[i];
-                pile[i] = pile[random];
-                pile[random] = temp;
-            }
-        }
-
-        //Return true if deck is empty
-        bool isEmpty() {
-            return false;
-        }
-};
 
 class Card{
     private:
@@ -130,71 +21,37 @@ class Card{
 
     public:
         // constructor to create a card, setting the suit and rank
-        Card::Card(char r, char s){
+        Card(char r, char s){
             rank = r;
             suit = s;
         }
 
         // display the card example: AC, 10S, KD
-        void Card::print(){
+        void print(){
             switch (rank) {
-                case 'A':
-                    cout << 'A';
-                    break;
-                case '2':
-                    cout << '2';
-                    break;
-                case '3':
-                    cout << '3';
-                    break;
-                case '4':
-                    cout << '4';
-                    break;
-                case '5':
-                    cout << '5';
-                    break;
-                case '6':
-                    cout <<'6';
-                    break;
-                case '7':
-                    cout << '7';
-                    break;
-                case '8':
-                    cout <<'8';
-                    break;
-                case '9':
-                    cout << '9';
-                    break;
-                case 'T':
-                    cout << "10";
-                    break;
-                case 'J':
-                    cout << 'J';
-                    break;
-                case 'Q':
-                    cout <<'Q';
-                    break;
-                case 'K':
-                    cout << 'K';
-                    break;
+                case 'A': cout << 'A'; break;
+                case '2': cout << '2'; break;
+                case '3': cout << '3'; break;
+                case '4': cout << '4'; break;
+                case '5': cout << '5'; break;
+                case '6': cout <<'6'; break;
+                case '7': cout << '7'; break;
+                case '8': cout <<'8'; break;
+                case '9': cout << '9'; break;
+                case 'T': cout << "10"; break;
+                case 'J': cout << 'J'; break;
+                case 'Q': cout <<'Q'; break;
+                case 'K': cout << 'K'; break;
             }
             switch (suit) {
-                case 'H':
-                    cout << 'H';
-                    break;
-                case 'D':
-                    cout << 'D';
-                    break;
-                case 'C':
-                    cout << 'C';
-                    break;
-                case 'S':
-                    cout << 'S';
-                    break;
+                case 'H': cout << 'H'; break;
+                case 'D': cout << 'D'; break;
+                case 'C': cout << 'C'; break;
+                case 'S': cout << 'S'; break;
             }
         }
 
-        int Card::compare(Card other){
+        int compare(Card other){
             char ranks[] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};        //creates an array of ranks in order
             char p1 = rank;
             char p2 = other.rank;
@@ -212,6 +69,86 @@ class Card{
             if (p1_index > p2_index) return 1;
             if (p1_index < p2_index) return -1;
             return 0;
+        }
+};
+
+class Deck{
+    private:
+        // use vector to store cards in deck
+        vector<Card> pile;
+        int topCard;
+    // Deck constructor. Creates a deck of 52 cards
+    public:
+        Deck(){
+            char rank;
+            char suit;
+            topCard = 52;
+        // use vector to store cards in deck
+            for (int s = 0; s < 4; s++) {
+                switch (s) {
+                    case 0: suit = 'H'; break;
+                    case 1: suit = 'D'; break;
+                    case 2: suit = 'C'; break;
+                    case 3: suit = 'S'; break;
+                }
+                for (int r = 0; r < 13; r++) {
+                    switch (r) {
+                        case 0: rank = 'A'; break;
+                        case 1: rank = '2'; break;
+                        case 2: rank = '3'; break;
+                        case 3: rank = '4'; break;
+                        case 4: rank = '5'; break;
+                        case 5: rank = '6'; break;
+                        case 6: rank = '7'; break;
+                        case 7: rank = '8'; break;
+                        case 8: rank = '9'; break;
+                        case 9: rank = 'T'; break;
+                        case 10: rank = 'J'; break;
+                        case 11: rank = 'Q'; break;
+                        case 12: rank = 'K'; break;
+                    }
+                    // Card currentCard(rank, suit);
+                    pile.push_back(Card(rank, suit));
+                }
+            }
+        }
+
+        // Deals a card by taking a card from the deck
+        // Deal a card if possible, otherwise Raise exception if # of games to play is > cards in deck
+        Card deal(){
+            if (pile.empty()){
+                throw runtime_error("Number of games exceeds cards in deck.");
+            }
+            Card dealtCard = pile.back();
+            pile.pop_back();
+            --topCard;
+            return dealtCard;
+        }
+
+        //Shows all the cards in the deck
+        void print(){
+            for (int i = 0; i < topCard; i++){
+                if (i%13 == 0 && i != 0){
+                    cout << "\n";
+                }
+                pile[i].print();
+                cout << " ";
+            }
+            cout << endl << endl;
+        }
+
+        //Shuffles the cards in the deck
+        void shuffle(){
+            for (int i = 0; i < topCard; i++){
+                int random = rand() % topCard;
+                Card temp = pile[i];
+                swap(pile[i], pile[random]);
+            }
+        }
+
+        //Return true if deck is empty
+        bool isEmpty() {
+            return pile.empty();
         }
 };
 
@@ -238,35 +175,41 @@ int main() {
     int p2_wins = 0;
     int ties = 0;
 
-    //Play 26 games
-    for (int i = 0; i < gamesToPlay; i++){
-        cout << "Game " << i + 1 << endl << "--------" << endl;
-        Card p1_card = myDeck.deal();
-        Card p2_card = myDeck.deal();
-        int result = p1_card.compare(p2_card);
-        //Anounce the winner (or tie) of EACH game
-        if (result == 1) {
-            cout << "\t" << player1 << "=>";
-            p1_card.print();
-            cout << endl << "\t" << player2 << "=>";
-            p2_card.print();
-            cout << endl << player1 << "=> Winner" << endl << endl;
-            p1_wins++;
-        } else if (result == -1) {
-            cout << "\t" << player1 << "=>";
-            p1_card.print();
-            cout << endl << "\t" << player2 << "=>";
-            p2_card.print();
-            cout << endl << player2 << "=> Winner" << endl << endl;
-            p2_wins++;
-        } else {
-            cout << "\t" << player1 << "=>";
-            p1_card.print();
-            cout << endl << "\t" << player2 << "=>";
-            p2_card.print();
-            cout << endl << "Tie game" << endl << endl;
-            ties++;
+    //Play x amount of games
+    //If games being played is greater than cards in deck, an exception will be thrown
+    try{
+        for (int i = 0; i < gamesToPlay; i++){
+            cout << "Game " << i + 1 << endl << "--------" << endl;
+            Card p1_card = myDeck.deal();
+            Card p2_card = myDeck.deal();
+            int result = p1_card.compare(p2_card);
+            //Anounce the winner (or tie) of EACH game
+            if (result == 1) {
+                cout << "\t" << player1 << "=>";
+                p1_card.print();
+                cout << endl << "\t" << player2 << "=>";
+                p2_card.print();
+                cout << endl << player1 << "=> Winner" << endl << endl;
+                p1_wins++;
+            } else if (result == -1) {
+                cout << "\t" << player1 << "=>";
+                p1_card.print();
+                cout << endl << "\t" << player2 << "=>";
+                p2_card.print();
+                cout << endl << player2 << "=> Winner" << endl << endl;
+                p2_wins++;
+            } else {
+                cout << "\t" << player1 << "=>";
+                p1_card.print();
+                cout << endl << "\t" << player2 << "=>";
+                p2_card.print();
+                cout << endl << "Tie game" << endl << endl;
+                ties++;
+            }
         }
+    }
+    catch (const exception& e){
+        cerr << "Exception caught: " << e.what() << endl;
     }
 
     //After 26 games are played, print the statistics
